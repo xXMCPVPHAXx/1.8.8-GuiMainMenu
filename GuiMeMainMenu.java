f894006d8cd7825ae0d4843cab65befd6918bc2c
xXMCPVPHAXx <ghostaplays@gmail.com>
2015-12-20 13:11:59 +0100

    Update GuiMeMainMenu.java

--- GuiMeMainMenu.java
+++ GuiMeMainMenu.java
@@ -45,37 +45,20 @@ public class GuiMeMainMenu extends GuiScreen implements GuiYesNoCallback
 {
     private static final AtomicInteger field_175373_f = new AtomicInteger(0);
     private static final Logger logger = LogManager.getLogger();
-    private static final Random RANDOM = new Random();
 
-    /** Counts the number of screen updates. */
-    private float updateCounter;
-
-    /** The splash message. */
-    private String splashText;
     private GuiButton buttonResetDemo;
 
-
-    /**
-     * Texture allocated for the current viewport of the main menu's panorama background.
-     */
     private DynamicTexture viewportTexture;
     private boolean field_175375_v = true;
 
-    /**
-     * The Object object utilized as a thread lock when performing non thread-safe operations
-     */
     private final Object threadLock = new Object();
 
-    /** OpenGL graphics card warning. */
     private String openGLWarning1;
 
-    /** OpenGL graphics card warning. */
     private String openGLWarning2;
 
-    /** Link to the Mojang Support about minimum requirements */
     private String openGLWarningLink;
 
-    /** An array of all the paths to the panorama pictures. */
     public static final String field_96138_a = "Please click " + EnumChatFormatting.UNDERLINE + "here" + EnumChatFormatting.RESET + " for more information.";
     private int field_92024_r;
     private int field_92023_s;
@@ -83,9 +66,7 @@ public class GuiMeMainMenu extends GuiScreen implements GuiYesNoCallback
     private int field_92021_u;
     private int field_92020_v;
     private int field_92019_w;
-    private ResourceLocation backgroundTexture;
 
-    /** Minecraft Realms button. */
     private GuiButton realmsButton;
 
     public GuiMeMainMenu()
@@ -93,7 +74,6 @@ public class GuiMeMainMenu extends GuiScreen implements GuiYesNoCallback
         this.openGLWarning2 = field_96138_a;
         BufferedReader bufferedreader = null;
 
-        this.updateCounter = RANDOM.nextFloat();
         this.openGLWarning1 = "";
 
         if (!GLContext.getCapabilities().OpenGL20 && !OpenGlHelper.areShadersSupported())
@@ -116,7 +96,6 @@ public class GuiMeMainMenu extends GuiScreen implements GuiYesNoCallback
     public void initGui()
     {
         this.viewportTexture = new DynamicTexture(256, 256);
-        this.backgroundTexture = this.mc.getTextureManager().getDynamicTextureLocation("background", this.viewportTexture);
         Calendar calendar = Calendar.getInstance();
         calendar.setTime(new Date());
 
@@ -125,7 +104,6 @@ public class GuiMeMainMenu extends GuiScreen implements GuiYesNoCallback
 
         if (this.mc.isDemo())
         {
-            this.addDemoButtons(j, 24);
         }
         else
         {
@@ -155,19 +133,6 @@ public class GuiMeMainMenu extends GuiScreen implements GuiYesNoCallback
         this.buttonList.add(new GuiButton(1, this.width / 2 - 100, p_73969_1_, I18n.format("menu.singleplayer", new Object[0])));
         this.buttonList.add(new GuiButton(2, this.width / 2 - 100, p_73969_1_ + p_73969_2_ * 1, I18n.format("menu.multiplayer", new Object[0])));
         this.buttonList.add(this.realmsButton = new GuiButton(14, this.width / 2 - 100, p_73969_1_ + p_73969_2_ * 2, I18n.format("menu.online", new Object[0])));
-    }
-
-    private void addDemoButtons(int p_73972_1_, int p_73972_2_)
-    {
-        this.buttonList.add(new GuiButton(11, this.width / 2 - 100, p_73972_1_, I18n.format("menu.playdemo", new Object[0])));
-        this.buttonList.add(this.buttonResetDemo = new GuiButton(12, this.width / 2 - 100, p_73972_1_ + p_73972_2_ * 1, I18n.format("menu.resetdemo", new Object[0])));
-        ISaveFormat isaveformat = this.mc.getSaveLoader();
-        WorldInfo worldinfo = isaveformat.getWorldInfo("Demo_World");
-
-        if (worldinfo == null)
-        {
-            this.buttonResetDemo.enabled = false;
-        }
     }
 
     protected void actionPerformed(GuiButton button) throws IOException
@@ -201,58 +166,12 @@ public class GuiMeMainMenu extends GuiScreen implements GuiYesNoCallback
         {
             this.mc.shutdown();
         }
-
-        if (button.id == 11)
-        {
-            this.mc.launchIntegratedServer("Demo_World", "Demo_World", DemoWorldServer.demoWorldSettings);
-        }
-
-        if (button.id == 12)
-        {
-            ISaveFormat isaveformat = this.mc.getSaveLoader();
-            WorldInfo worldinfo = isaveformat.getWorldInfo("Demo_World");
-
-            if (worldinfo != null)
-            {
-                GuiYesNo guiyesno = GuiSelectWorld.func_152129_a(this, worldinfo.getWorldName(), 12);
-                this.mc.displayGuiScreen(guiyesno);
-            }
-        }
     }
 
     private void switchToRealms()
     {
         RealmsBridge realmsbridge = new RealmsBridge();
         realmsbridge.switchToRealms(this);
-    }
-
-    public void confirmClicked(boolean result, int id)
-    {
-        if (result && id == 12)
-        {
-            ISaveFormat isaveformat = this.mc.getSaveLoader();
-            isaveformat.flushCache();
-            isaveformat.deleteWorldDirectory("Demo_World");
-            this.mc.displayGuiScreen(this);
-        }
-        else if (id == 13)
-        {
-            if (result)
-            {
-                try
-                {
-                    Class<?> oclass = Class.forName("java.awt.Desktop");
-                    Object object = oclass.getMethod("getDesktop", new Class[0]).invoke((Object)null, new Object[0]);
-                    oclass.getMethod("browse", new Class[] {URI.class}).invoke(object, new Object[] {new URI(this.openGLWarningLink)});
-                }
-                catch (Throwable throwable)
-                {
-                    logger.error("Couldn\'t open link", throwable);
-                }
-            }
-
-            this.mc.displayGuiScreen(this);
-        }
     }
 
     public void drawScreen(int mouseX, int mouseY, float partialTicks)
@@ -277,15 +196,9 @@ public class GuiMeMainMenu extends GuiScreen implements GuiYesNoCallback
         GlStateManager.translate((float)(this.width / 2 + 90), 70.0F, 0.0F);
         GlStateManager.rotate(-20.0F, 0.0F, 0.0F, 1.0F);
         float f = 1.8F - MathHelper.abs(MathHelper.sin((float)(Minecraft.getSystemTime() % 1000L) / 1000.0F * (float)Math.PI * 2.0F) * 0.1F);
-        f = f * 100.0F / (float)(this.fontRendererObj.getStringWidth(this.splashText) + 32);
         GlStateManager.scale(f, f, f);
         GlStateManager.popMatrix();
         String s = "Minecraft 1.8.8";
-
-        if (this.mc.isDemo())
-        {
-            s = s + " Demo";
-        }
 
         this.drawString(this.fontRendererObj, s, 2, this.height - 10, -1);
         String s1 = "Copyright Mojang AB. Do not distribute!";
@@ -301,18 +214,4 @@ public class GuiMeMainMenu extends GuiScreen implements GuiYesNoCallback
         super.drawScreen(mouseX, mouseY, partialTicks);
     }
 
-    protected void mouseClicked(int mouseX, int mouseY, int mouseButton) throws IOException
-    {
-        super.mouseClicked(mouseX, mouseY, mouseButton);
-
-        synchronized (this.threadLock)
-        {
-            if (this.openGLWarning1.length() > 0 && mouseX >= this.field_92022_t && mouseX <= this.field_92020_v && mouseY >= this.field_92021_u && mouseY <= this.field_92019_w)
-            {
-                GuiConfirmOpenLink guiconfirmopenlink = new GuiConfirmOpenLink(this, this.openGLWarningLink, 13, true);
-                guiconfirmopenlink.disableSecurityWarning();
-                this.mc.displayGuiScreen(guiconfirmopenlink);
-            }
-        }
-    }
 }
